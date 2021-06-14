2ffa6de48116c17afef5b67847f64c7c65ce6aaa
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-06-14 16:39:55 -0300

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from  'react';
+import Navbar from './components/Navbar';
+import GlobalStyle from './GlobalStyles';
+
+function App() {
+  return (
+   <>
+    <GlobalStyle />
+    <Navbar />
+   </>
+  );
+}
+
+export default App;
