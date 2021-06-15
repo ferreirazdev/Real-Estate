273828e1785a90236e9686cd7721de085622ffce
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-06-15 08:49:08 -0300

    Image Slider

---
+++ src/data/SliderData.js
@@ -0,0 +1,66 @@
+import Image1 from '../images/house1.jpg';
+import Image2 from '../images/house2.jpg';
+import Image3 from '../images/house3.jpg';
+import Image4 from '../images/house4.jpg';
+
+export const SliderData = [
+  {
+    title: 'Luxury Villa in Bali, Indonesia',
+    price: '$4,280,000',
+    path: '/homes',
+    label: 'View Home',
+    image: Image1,
+    alt: 'House'
+  },
+  {
+    title: 'House in San Jose, Costa Rica',
+    price: '$2,860,000',
+    path: '/homes',
+    label: 'View Home',
+    image: Image2,
+    alt: 'House'
+  },
+  {
+    title: 'House in Calabasas, California',
+    price: '$8,420,000',
+    path: '/homes',
+    label: 'View Home',
+    image: Image3,
+    alt: 'House'
+  },
+  {
+    title: 'House in Houston, Texas',
+    price: '$6,660,000',
+    path: '/homes',
+    label: 'View Home',
+    image: Image4,
+    alt: 'House'
+  }
+];
+
+// export const SliderDataTwo = [
+//   {
+//     title: 'House in Las Vegas, Nevada',
+//     price: '$8,890,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageFive,
+//     alt: 'House'
+//   },
+//   {
+//     title: 'House in Houston, Texas',
+//     price: '$3,460,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageSix,
+//     alt: 'House'
+//   },
+//   {
+//     title: 'House in Alajuela, Costa Rica',
+//     price: '$5,420,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageSeven,
+//     alt: 'House'
+//   },
+// ];
